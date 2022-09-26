6e2ca26177b5e84d067c3edbe0eb0e8ec9b868e7
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-09-26 13:15:48 -0600

    a

--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,4 @@ import App from './components/App'
 import './index.sass'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const myname = 'ROBERTO RIOS...'
-ReactDOM.render(<App name={myname} />, document.getElementById('app'))
+ReactDOM.render(<App name="ROBERTO RIOS" />, document.getElementById('app'))
