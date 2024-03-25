22f98893d6778a268359fbe9202d2b60edc287a4
Roberto Rios <87707371+robertriosm@users.noreply.github.com>
2024-03-25 14:14:30 -0600

    trial

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/jest.config.js',
-    '\\.(css|less|sass)$': '<rootDir>/jest.config.js',
-  },
-}
--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
-import './index.sass'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-const myname = 'ROBERTO RIOS...'
-ReactDOM.render(<App name={myname} />, document.getElementById('app'))
